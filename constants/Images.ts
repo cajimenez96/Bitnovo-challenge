63272bc1242424f91e2b2fd03e252dd013bc1a4f
cajimenez96 <carlosj_utn@hotmail.com>
2025-02-26 09:12:49 -0300

    Update images

--- constants/Images.ts
+++ constants/Images.ts
@@ -1,11 +1,28 @@
+interface GenericIcons {
+  [key: string]: string;
+}
+
 export const Images = {
   euro: require('../assets/icons/eur.png'),
   gbp: require('../assets/icons/gbp.png'),
   usa: require('../assets/icons/usa.png'),
+  cardPayment: require('../assets/icons/cardPayment.png'),
+  linkIcon: require('../assets/icons/link.png'),
+  exportIcon: require('../assets/icons/export.png'),
+  emailIcon: require('../assets/icons/sms.png'),
+  whatsappIcon: require('../assets/icons/whatsapp.png'),
+  newOrder: require('../assets/icons/wallet-add.png'),
 }
 
-export const countryFlag = {
+export const countryFlag: GenericIcons = {
   'EU': Images.euro,
   'GB': Images.gbp,
   'US': Images.usa,
+}
+
+export const shareButtonIcon: GenericIcons = {
+  'link': Images.linkIcon,
+  'email': Images.emailIcon,
+  'whatsapp': Images.whatsappIcon,
+  'others': Images.exportIcon
 }
