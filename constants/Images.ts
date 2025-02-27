3cfec0af365d023c8ebcbb49662614bb19c2691f
cajimenez96 <carlosj_utn@hotmail.com>
2025-02-27 10:47:42 -0300

    Add countries

--- constants/Images.ts
+++ constants/Images.ts
@@ -3,9 +3,17 @@ interface GenericIcons {
 }
 
 export const Images = {
-  euro: require('../assets/icons/eur.png'),
-  gbp: require('../assets/icons/gbp.png'),
-  usa: require('../assets/icons/usa.png'),
+  euro: require('../assets/countries/eur.png'),
+  gbp: require('../assets/countries/gbp.png'),
+  usa: require('../assets/countries/usa.png'),
+  esp: require('../assets/countries/esp.png'),
+  equatorial: require('../assets/countries/equatorial.png'),
+  grecia: require('../assets/countries/grecia.png'),
+  guatemala: require('../assets/countries/guatemala.png'),
+  guyana: require('../assets/countries/guyana.png'),
+  honduras: require('../assets/countries/honduras.png'),
+  hongkong: require('../assets/countries/hongkong.png'),
+
   cardPayment: require('../assets/icons/cardPayment.png'),
   linkIcon: require('../assets/icons/link.png'),
   exportIcon: require('../assets/icons/export.png'),
@@ -21,6 +29,13 @@ export const countryFlag: GenericIcons = {
   'EU': Images.euro,
   'GB': Images.gbp,
   'US': Images.usa,
+  'HN': Images.honduras,
+  'HK': Images.hongkong,
+  'GQ': Images.equatorial,
+  'GR': Images.grecia,
+  'GY': Images.guyana,
+  'GT': Images.guatemala,
+  'ES': Images.esp
 }
 
 export const shareButtonIcon: GenericIcons = {
