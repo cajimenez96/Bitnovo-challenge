f4fb6f95120d7a87c37aaa8445bda9d3d25cb6ec
cajimenez96 <carlosj_utn@hotmail.com>
2025-02-27 10:47:50 -0300

    Add validations

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,2 +1,7 @@
 export const MAX_CHARACTERS = 140;
 export const WS = 'wss://payments.pre-bnvo.com/ws/merchant/';
+
+export const validatePhone = (phone: string): boolean => {
+  const regex = /^\+?[1-9]\d{7,14}$/;
+  return regex.test(phone);
+}
