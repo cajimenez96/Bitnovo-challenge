6d276395e295efe75ec03cb01fa2a911f1887ddc
cajimenez96 <carlosj_utn@hotmail.com>
2025-02-27 10:48:02 -0300

    Add code countries

---
+++ constants/Countries.ts
@@ -0,0 +1,10 @@
+export const Countries = [
+  {name: 'ES', code: '+34', text: 'España'},
+  {name: 'GQ', code: '+240', text: 'Equatorial Guinea'},
+  {name: 'GR', code: '+30', text: 'Grecia'},
+  {name: 'GB', code: '+500', text: 'South Georgia and the S...'},
+  {name: 'GT', code: '+502', text: 'Guatemala'},
+  {name: 'GY', code: '+592', text: 'Guyana'},
+  {name: 'HK', code: '+852', text: 'Hong Kong'},
+  {name: 'HN', code: '+504', text: 'Honduras'},
+]
